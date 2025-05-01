6a5d407a2f8280c4ba253c74e033123b54fd3db5
Islam <islam@inaya.edu.sa>
2025-05-01 11:19:42 +0300

    adding custom error message for 404

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use App\Http\Middleware\Api\AlwaysAcceptJson;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +19,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->prepend(AlwaysAcceptJson::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $exception, Request $request) {
+            if ($request->wantsJson()) {
+                return response()->json([
+                    'message' => 'Object not found',
+                ], 404);
+            }
+        });
     })->create();
